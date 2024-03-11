9421cc727357051bd8f275ac0e9994370d8e8430
0x-sudo <rahul_chaudhary@tutanota.com>
2024-03-11 23:49:00 +0530

    17. optimal, moore's voting algorithm

--- arrayz/medium/occurs_more_than_half.cpp
+++ arrayz/medium/occurs_more_than_half.cpp
@@ -20,20 +20,48 @@ using namespace std;
 // }
 
 // Hashing O(N * logN)
+// int majorityElement(vector<int> &arr) {
+//   int n = arr.size();
+//
+//   map<int, int> mpp;
+//
+//   for (int i = 0; i < n; i++) {
+//     mpp[arr[i]]++;
+//   }
+//
+//   for (auto it : mpp) {
+//     if (it.second > (n / 2)) {
+//       return it.first;
+//     }
+//   }
+//   return -1;
+// }
+
+// Moore's voting algorithm O(N) + O(N)
 int majorityElement(vector<int> &arr) {
   int n = arr.size();
-
-  map<int, int> mpp;
+  int cnt = 0;
+  int el;
 
   for (int i = 0; i < n; i++) {
-    mpp[arr[i]]++;
+    if (cnt == 0) {
+      cnt = 1;
+      el = arr[i];
+    } else if (el == arr[i])
+      cnt++;
+    else
+      cnt--;
   }
 
-  for (auto it : mpp) {
-    if (it.second > (n / 2)) {
-      return it.first;
-    }
+  int cnt1 = 0;
+  for (int i = 0; i < n; i++) {
+    if (arr[i] == el)
+      cnt1++;
   }
+
+  if (cnt1 > (n / 2))
+    return el;
+
   return -1;
 }
 
